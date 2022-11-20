149b5b072f286ea3e9198468ccc90117952d8e89
crislainesc <criis.santos@outlook.com>
2022-11-20 15:26:12 -0300

    chore: change default locale

--- config/application.rb
+++ config/application.rb
@@ -24,14 +24,10 @@ module EcommerceApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    # I18n config
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+    config.i18n.default_locale = :'pt-BR'
+  
     config.api_only = true
   end
 end
