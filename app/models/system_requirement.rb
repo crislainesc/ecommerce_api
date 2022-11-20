5e598e6aa1b60e9697d4f074bcee1e418a68789e
crislainesc <criis.santos@outlook.com>
2022-11-20 16:30:34 -0300

    feat: add game model

--- app/models/system_requirement.rb
+++ app/models/system_requirement.rb
@@ -1,5 +1,7 @@
 class SystemRequirement < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  has_many :games, dependent: :restrict_with_error
+
   validates :operational_system, presence: true
   validates :storage, presence: true
   validates :processor, presence: true
