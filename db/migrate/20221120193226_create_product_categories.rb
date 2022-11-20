efa754326c27e9775eea7da836b7785a5448c6f5
crislainesc <criis.santos@outlook.com>
2022-11-20 16:37:57 -0300

    feat: add product_category model

---
+++ db/migrate/20221120193226_create_product_categories.rb
@@ -0,0 +1,10 @@
+rails db:migrateclass CreateProductCategories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :product_categories do |t|
+      t.references :product, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
