d88cdfffc005938c07757f35c30ec6382eb918b4
crislainesc <criis.santos@outlook.com>
2022-11-20 16:00:04 -0300

    test: add system requirement model

---
+++ spec/factories/system_requirements.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :system_requirement do
+    name { "MyString" }
+    operational_system { "MyString" }
+    string { "MyString" }
+    storage { "" }
+    processor { "MyString" }
+    memory { "MyString" }
+    video_board { "MyString" }
+  end
+end
